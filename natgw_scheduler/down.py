4ec41668a0793b0551a37d94addcb16f21b7cdeb
ishibashi-io <takashi.ishibashi@industry-one.com>
2024-08-08 17:01:55 +0900

    fix val name

--- natgw_scheduler/down.py
+++ natgw_scheduler/down.py
@@ -2,19 +2,19 @@ import os
 
 import boto3
 
-client = boto3.client('ec2')
+ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     rtb_id = os.environ['RTB_ID']
 
-    response = client.delete_route(
+    response = ec2.delete_route(
         DestinationCidrBlock = '0.0.0.0/0',
         RouteTableId = rtb_id
     )
 
     filters = [{'Name': 'state', 'Values': ['available']}]
-    response = client.describe_nat_gateways(Filters=filters)
+    response = ec2.describe_nat_gateways(Filters=filters)
     for rec in response['NatGateways']:
         natgw = rec['NatGatewayId']
-        client.delete_nat_gateway(NatGatewayId=natgw)
+        ec2.delete_nat_gateway(NatGatewayId=natgw)
 
