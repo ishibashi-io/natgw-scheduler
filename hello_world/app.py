29e1959d714793f5e87a1574528047e030dd0988
ishibashi-io <takashi.ishibashi@industry-one.com>
2024-08-08 15:39:15 +0900

    add hello world app

---
+++ hello_world/app.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+# import requests
+
+
+def lambda_handler(event, context):
+    response = requests.get("https://www.industry-one.com/")
+    if response.status_code != 200:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({
+                "message": "failed to get industry-one.com",
+            }),
+        }
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world",
+        }),
+    }
